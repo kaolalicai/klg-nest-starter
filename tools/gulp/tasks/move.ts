b82e04b11b51404e86a066a5d2b7de148aeee3f7
Nick <myfjdthink@163.com>
2020-04-17 11:41:12 +0800

    gulp move common file

--- tools/gulp/tasks/move.ts
+++ tools/gulp/tasks/move.ts
@@ -1,26 +1,14 @@
 import {task, src, dest} from 'gulp'
+import * as rename from 'gulp-rename'
 import {getDirs} from '../util/task-helpers'
 import {samplePath} from '../config'
-import {join} from 'path'
 
 /**
- * Moves the compiled aka files into the
- * `samples/*` and `integration/*` dirs.
+ * Moves the base config filesfiles into the
+ * `samples/*` dirs.
  */
-function move () {
-  const directories = getDirs(samplePath)
-
-  const distFiles = src(['node_modules/@akajs/**/*'])
-
-  return directories.reduce(
-    (distFile, dir) => distFile.pipe(dest(join(dir, '/node_modules/@akajs'))),
-    distFiles
-  )
-}
-
 function moveCommonFile () {
   const directories = getDirs(samplePath)
-
   const distFiles = src([
     './.gitignore',
     './.dockerignore',
@@ -28,14 +16,18 @@ function moveCommonFile () {
     './README_BASE.md',
     './tslint.json',
     './tsconfig.json',
-    './tsconfig.build.json',
+    './tsconfig.build.json'
   ])
-
   return directories.reduce(
-    (distFile, dir) => distFile.pipe(dest(dir)),
+    (distFile, dir) => {
+      console.log('distFile', distFile)
+      return distFile.pipe(rename(function (path) {
+        console.log('path', path)
+        path.basename = path.basename.replace('_BASE', '')
+      })).pipe(dest(dir))
+    },
     distFiles
   )
 }
 
-task('move:common', moveCommonFile)
-task('move', move)
+task('move', moveCommonFile)
