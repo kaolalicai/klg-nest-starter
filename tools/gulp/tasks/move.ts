708c85cc73e005b903d1f379a5414c55013d1e80
Nick <myfjdthink@163.com>
2020-04-20 10:23:41 +0800

    task :move

--- tools/gulp/tasks/move.ts
+++ tools/gulp/tasks/move.ts
@@ -1,14 +1,32 @@
 import {task, src, dest, series} from 'gulp'
 import {getDirs} from '../util/task-helpers'
+import {samplePath} from '../config'
+import {join} from 'path'
 
 /**
  * Moves the hello-world project into the
  * `boilerplate/*` dirs.
  */
-function move () {
+function moveBoilerplate () {
   return src(['sample/hello-world/**/*', '!sample/hello-world/node_modules/**/*', '!sample/hello-world/dist/**/*'])
     .pipe(dest('template/boilerplate/'))
 }
 
-task('move:boilerplate', move)
+/**
+ * Moves the compiled aka files into the
+ * `samples/*` and `integration/*` dirs.
+ */
+function move () {
+  const samplesDirs = getDirs(samplePath)
+2
+  const distFiles = src(['node_modules/@kalengo/**/*'])
+
+  return samplesDirs.reduce(
+    (distFile, dir) => distFile.pipe(dest(join(dir, '/node_modules/@kalengo'))),
+    distFiles
+  )
+}
+
+task('move:boilerplate', moveBoilerplate)
 task('move:template', series('clean:template', 'move:boilerplate'))
+task('move', move)
